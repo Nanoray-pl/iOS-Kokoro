2579fd944af421d2da182863203e520215514627
Shockah <me@shockah.pl>
2021-11-05 12:15:11 +0100

    [UI] Routing refactor

--- KokoroUI/Business Logic/Routing.swift
+++ KokoroUI/Business Logic/Routing.swift
@@ -3,16 +3,6 @@
 //  Copyright © 2021 Nanoray. All rights reserved.
 //
 
-#if canImport(UIKit)
-import UIKit
-
-public protocol Router: AnyObject {
-	var parentRouter: Router? { get }
-	var childRouters: [Router] { get }
-}
-
-public protocol Route: AnyObject {}
-
 public enum RoutingDirection {
 	/// Route via the first known upstream (parent) router. Best used if context is known (simple navigation) - for example with navigation controllers.
 	case upstream
@@ -24,25 +14,41 @@ public enum RoutingDirection {
 	case anyDirection
 }
 
-public extension UIResponder {
-	/// Returns the first `Router` found in the `UIResponder` chain. Best used in simple navigation (when you are sure the router still exists, for example with direct touch handlers).
-	var firstRouter: Router? {
-		var current = self
-		while true {
-			if let router = current as? Router {
-				return router
-			}
-			if let next = current.next {
-				current = next
-			} else {
-				return nil
-			}
-		}
+public protocol RouterAware: AnyObject {
+	func optionalRouter<RouteType: Route>(_ direction: RoutingDirection, for routeType: RouteType.Type) -> RouteType?
+
+	/// - Warning: This method will call `fatalError` if it cannot find the specific router.
+	func router<RouteType: Route>(_ direction: RoutingDirection, for routeType: RouteType.Type) -> RouteType
+
+	/// - Warning: This method will call `fatalError` if it cannot find the specific router.
+	func route<RouteType: Route>(_ direction: RoutingDirection, via routeType: RouteType.Type, routing: (RouteType) -> Void)
+}
+
+public extension RouterAware {
+	func optionalRouter<RouteType: Route>(for routeType: RouteType.Type) -> RouteType? {
+		return optionalRouter(.anyDirection, for: routeType)
 	}
 
-	/// Returns the root `Router` of this `UIResponder` chain, as in the one with no parent. Best used in complex navigation (when you are not sure `firstRouter` would still exist, for example after receiving a HTTP response).
+	/// - Warning: This method will call `fatalError` if it cannot find the specific router.
+	func router<RouteType: Route>(for routeType: RouteType.Type) -> RouteType {
+		return router(.anyDirection, for: routeType)
+	}
+
+	/// - Warning: This method will call `fatalError` if it cannot find the specific router.
+	func route<RouteType: Route>(via routeType: RouteType.Type, routing: (RouteType) -> Void) {
+		return route(.anyDirection, via: routeType, routing: routing)
+	}
+}
+
+public protocol FirstRouterAware: RouterAware {
+	/// Returns the first `Router` found in the chain. Best used in simple navigation (when you are sure the router still exists, for example with direct touch handlers).
+	var firstRouter: Router? { get }
+}
+
+public extension FirstRouterAware {
+	/// Returns the root `Router` of this router chain, as in the one with no parent. Best used in complex navigation (when you are not sure `firstRouter` would still exist, for example after receiving a HTTP response).
 	var rootRouter: Router? {
-		guard var current = firstRouter else { return nil }
+		guard var current: Router = firstRouter else { return nil }
 		while true {
 			if let next = current.parentRouter {
 				current = next
@@ -53,23 +59,31 @@ public extension UIResponder {
 		return current
 	}
 
-	func optionalRouter<RouteType>(_ direction: RoutingDirection = .anyDirection, for routeType: RouteType.Type) -> RouteType? {
+	func optionalRouter<RouteType>(_ direction: RoutingDirection, for routeType: RouteType.Type) -> RouteType? {
 		return firstRouter?.optionalRouter(direction, for: routeType)
 	}
 
-	/// - Warning: This method will call `fatalError` if it cannot find the specific router.
-	func router<RouteType>(_ direction: RoutingDirection = .anyDirection, for routeType: RouteType.Type) -> RouteType {
-		guard let firstRouter = firstRouter else { fatalError("\(self) does not have a `Router` in its `UIResponder` chain.") }
+	func router<RouteType>(_ direction: RoutingDirection, for routeType: RouteType.Type) -> RouteType {
+		guard let firstRouter = firstRouter else { fatalError("\(self) does not have a `Router` in its chain.") }
 		return firstRouter.router(direction, for: routeType)
 	}
 
-	func route<RouteType>(_ direction: RoutingDirection = .anyDirection, via routeType: RouteType.Type, routing: (RouteType) -> Void) {
-		guard let firstRouter = firstRouter else { fatalError("\(self) does not have a `Router` in its `UIResponder` chain.") }
+	func route<RouteType>(_ direction: RoutingDirection, via routeType: RouteType.Type, routing: (RouteType) -> Void) {
+		guard let firstRouter = firstRouter else { fatalError("\(self) does not have a `Router` in its chain.") }
 		firstRouter.route(direction, via: routeType, routing: routing)
 	}
 }
 
+public protocol Router: FirstRouterAware {
+	var parentRouter: Router? { get }
+	var childRouters: [Router] { get }
+}
+
 public extension Router {
+	var firstRouter: Router? {
+		return self
+	}
+
 	private func firstUpstreamRouter<RouteType>(for routeType: RouteType.Type) -> RouteType? {
 		return (self as? RouteType) ?? parentRouter?.firstUpstreamRouter(for: routeType)
 	}
@@ -86,7 +100,7 @@ public extension Router {
 		return nil
 	}
 
-	func optionalRouter<RouteType>(_ direction: RoutingDirection = .anyDirection, for routeType: RouteType.Type) -> RouteType? {
+	func optionalRouter<RouteType>(_ direction: RoutingDirection, for routeType: RouteType.Type) -> RouteType? {
 		let optionalRouter: RouteType?
 		switch direction {
 		case .upstream:
@@ -99,14 +113,58 @@ public extension Router {
 		return optionalRouter
 	}
 
-	/// - Warning: This method will call `fatalError` if it cannot find the specific router.
-	func router<RouteType>(_ direction: RoutingDirection = .anyDirection, for routeType: RouteType.Type) -> RouteType {
+	func router<RouteType>(_ direction: RoutingDirection, for routeType: RouteType.Type) -> RouteType {
 		guard let router = optionalRouter(direction, for: routeType) else { fatalError("No known router handling \(routeType).") }
 		return router
 	}
 
-	func route<RouteType>(_ direction: RoutingDirection = .anyDirection, via routeType: RouteType.Type, routing: (RouteType) -> Void) {
+	func route<RouteType>(_ direction: RoutingDirection, via routeType: RouteType.Type, routing: (RouteType) -> Void) {
 		routing(router(direction, for: routeType))
+	}
+}
+
+public protocol Route: Router {}
+
+#if canImport(UIKit)
+import UIKit
+
+public protocol RouterAwareUIResponder: FirstRouterAware {}
+
+public extension RouterAwareUIResponder where Self: UIResponder {
+	/// Returns the first `Router` found in the `UIResponder` chain. Best used in simple navigation (when you are sure the router still exists, for example with direct touch handlers).
+	var firstRouter: Router? {
+		var current: UIResponder = self
+		while true {
+			if let router = current as? Router {
+				return router
+			} else if current != self, let firstRouterAware = current as? FirstRouterAware {
+				return firstRouterAware.firstRouter
+			} else if let next = current.next {
+				current = next
+			} else {
+				return nil
+			}
+		}
+	}
+}
+
+public protocol RouterAwareUIViewController: FirstRouterAware {}
+
+public extension RouterAwareUIViewController where Self: UIViewController {
+	/// Returns the first `Router` found in the `UIViewController` chain. Best used in simple navigation (when you are sure the router still exists, for example with direct touch handlers).
+	var firstRouter: Router? {
+		var current: UIViewController = self
+		while true {
+			if let router = current as? Router {
+				return router
+			} else if current != self, let firstRouterAware = current as? FirstRouterAware {
+				return firstRouterAware.firstRouter
+			} else if let next = current.parent {
+				current = next
+			} else {
+				return nil
+			}
+		}
 	}
 }
 #endif
